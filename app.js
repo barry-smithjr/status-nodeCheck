69fd1e2f172570c224d5a7b7dd4d24c7454a7a24
Barry Smith <barry.smithjr@gmail.com>
2018-01-14 04:09:53 -0600

    Try and load a config, die if missing

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ console.log('Starting HTTP status checker')
 
 // Required modules
 var http = require('http');
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+// Attempt to load config
+try {
+  const config = yaml.safeLoad(fs.readFileSync('sites.yml', 'utf8'));
+} catch (e) {
+  console.log(e);
+  console.log('Failed to load config, exiting');
+  process.exit(1);
+} finally {
+
+}
